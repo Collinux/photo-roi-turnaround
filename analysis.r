4ea140ef987e979277cacd719b6908430de163c6
Collin Guarino <collin.guarino@gmail.com>
2017-11-12 18:58:07 -0500

    Added analysis in R.

---
+++ analysis.r
@@ -0,0 +1,43 @@
+# analysis.r
+# Analyzes image metadata from the "popular" section of 500px.com
+#
+# Collin Guarino
+# The University of North Carolina Greensboro - Fall 2017
+# License: MIT
+#
+
+# Import the data and remove elapsed days greater than 1 year
+photos <- read.table("~/dev/py/photo-roi-turnaround/photos.dat", header=T)
+photos <- photos[which(photos$Elapsed.Days <= 365),]
+
+# Show a boxplot for the data without outliers
+categories <- photos$Category
+elapsed <- photos$Elapsed.Days
+boxplot(elapsed~categories,
+        names=c("Sport", "Journalism", "Landscapes", "Travel"),
+        main="Photographs Taken", xlab="Image Category",
+        ylab="Days Taken To Upload", outline = F)
+
+# Separate each category into its own vector
+sport <- photos[which(photos$Category == "Sport"),]$Elapsed.Days
+journalism <- photos[which(photos$Category == "Journalism"),]$Elapsed.Days
+landscapes <- photos[which(photos$Category == "Landscapes"),]$Elapsed.Days
+travel <- photos[which(photos$Category == "Travel"),]$Elapsed.Days
+
+# Remove outliers from each category
+sport <- sport[!sport %in% boxplot.stats(sport)$out]
+journalism <- journalism[!journalism %in% boxplot.stats(journalism)$out]
+landscapes <- landscapes[!landscapes %in% boxplot.stats(landscapes)$out]
+travel <- travel[!travel %in% boxplot.stats(travel)$out]
+
+# Confidence Intervals
+t.test(c(sport, journalism, landscapes,travel))$conf.int # Across all categories
+intervals <- c(t.test(sport)$conf.int, t.test(journalism)$conf.int, 
+               t.test(landscapes)$conf.int, t.test(travel)$conf.int)
+boxplot(intervals, xlab="Days Taken To Upload", horizontal = T)
+
+# Test each hypothesis
+t.test(journalism, landscapes, alternative = "less")
+t.test(landscapes, sport, alternative = "less")
+t.test(sport, travel, alternative = "less")
+
